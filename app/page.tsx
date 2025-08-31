47d71d7255d062df8082abe77fe371606ad87f01
Oleksandr Mas <sanya.maslovskii@gmail.com>
2025-08-31 14:21:57 +0300

    add About and Contact pages, implement footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
-export default function HomePage() {
+import Guest from "@/components/Guest";
+import { currentUser } from "@clerk/nextjs/server"
+
+export default async function HomePage() {
+  const user = await currentUser();
+  if(!user) {
+    return <Guest />
+  }
+
   return (
     <div className="text-red-500">Home</div>
   )
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,7 +14,7 @@ export default async function Navbar() {
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <Link href="/" className="flex-shrink-0">
-              <span className="text-md sm:text-2xl font-bold bg-gradient-to-l from-pink-600 to-blue-900 bg-clip-text text-transparent">
+              <span className="text-md sm:text-2xl font-bold bg-gradient-to-l from-orange-400 to-orange-800 bg-clip-text text-transparent">
                 SleepTracker
               </span>
             </Link>
@@ -37,7 +37,7 @@ export default async function Navbar() {
 
             <SignedOut>
               <SignInButton>
-                <button className="w-full sm:w-auto bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 hover:from-purple-600 hover:via-pink-600 hover:to-red-600 text-white sm:px-4 sm:py-2 px-3 py-1 text-sm sm:text-md rounded-md font-medium cursor-pointer">
+                <button className="w-full sm:w-auto bg-gradient-to-l from-orange-400 to-orange-800 hover:from-orange-300 hover:to-red-600 text-white sm:px-4 sm:py-2 px-3 py-1 text-sm sm:text-md rounded-md font-medium cursor-pointer">
                   Sign In
                 </button>
               </SignInButton>
